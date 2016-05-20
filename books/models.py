179790fad900d3e973c5ac84da77008c9332e566
Mariia <lmarijka.v@gmail.com>
2016-05-20 17:22:51 +0300

    Create custom BookAdmin

--- books/models.py
+++ books/models.py
@@ -6,18 +6,18 @@ class Book(models.Model):
     title = models.CharField(max_length=150)
     authors = models.ManyToManyField("Author", related_name="books")
     review = models.TextField(blank=True, null=True)
-    data_reviewed = models.DateTimeField(blank=True, null=True)
+    date_reviewed = models.DateTimeField(blank=True, null=True)
     is_favourite = models.BooleanField(default=False,verbose_name="Favourite?")
 
     def __str__(self):
-        return "{} by {}".format(self.title, self.list_author())
+        return "{} by {}".format(self.title, self.list_authors())
 
-    def list_author(self):
+    def list_authors(self):
         return ", ".join([author.name for author in self.authors.all()])
 
     def save(self, *args, **kwargs):
-        if (self.review and self.data_reviewed is None):
-            self.data_reviewed = now()
+        if (self.review and self.date_reviewed is None):
+            self.date_reviewed = now()
 
         super(Book, self).save(*args, **kwargs)
 
