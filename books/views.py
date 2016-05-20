3c04bc0382682b79acbe1150436609f622378570
Mariia <lmarijka.v@gmail.com>
2016-05-20 21:57:53 +0300

    Querying Our Data And Using It In Our View

--- books/views.py
+++ books/views.py
@@ -1,8 +1,19 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def list_books(request):
 #    return HttpResponse("We could put anything here")
 #    return HttpResponse(request.user.username)
-    return render(request, "list.html")
+
+    """
+    List the books that have reviews
+    """
+
+    books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+
+    context = {
+        'books':books,
+    }
+    return render(request, "list.html", context)
